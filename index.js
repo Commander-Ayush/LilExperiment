6c7c9504af2ce5523d09d563810e0e623b17eb05
Commander-Ayush <ayushkrjha221004@gmail.com>
2026-05-16 23:48:48 +0530

    fixing index and js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const params = new URLSearchParams(window.location.search);
 
 const businessName = params.get('name');   // "Mike's Plumbing"
 const city = params.get('city');           // "Austin"
+const service = pararm.get('service');
 
 document.getElementById('value1').textContent = businessName;
 document.getElementById('value2').textContent = city;
+document.getElementById('value3').textContent = params.get('service');
